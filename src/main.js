9092e84cf8f8ff42b90cd6fb1a0c85fa70591f87
Phil Laptop <phil@fyll.co.uk>
2017-03-12 17:48:21 +0000

    Add bill navigator

--- src/main.js
+++ src/main.js
@@ -8,8 +8,6 @@ var config = {
   apiURL: window.location.origin + '/api'
 }
 
-
-
 Vue.component('customer-section', {
   template: `
     <div v-if="customer">
@@ -28,49 +26,55 @@ Vue.component('customer-section', {
       </div>
     </div>
   `,
-  data: () => ({
-    customer: null
-  }),  
-  created: function () {
-    this.fetchData()
-  },  
-  methods: {
-    fetchData() {
-      var route = config.apiURL + '/customer.json';
-
-      this.$http.get(route).then((response) => {
-        this.customer = response.body.customer;
-      });
-    }
-  }  
+  props: ["customer"]
 })
 
 Vue.component('bills-section', {
   template: `
     <div v-if="bills">
       <p class="title">Your latest bills...</p>
+      <div v-if="bills[loadBill]" v-bind:loadBill="loadBill">
+        <table style="margin-bottom: 20px;">
+          <thead>
+            <tr>
+              <th>Total</th>
+              <th>Bill Date</th>              
+              <th>Usage</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr>
+              <td>{{ bills[loadBill].total }}</td>
+              <td>{{ bills[loadBill].billDate }}</td>
+              <td>
+                <ul>
+                  <li>Minutes: {{ bills[loadBill].used.minutes }}</li>
+                  <li>SMS: {{ bills[loadBill].used.text }}</li>
+                  <li>Data: {{ bills[loadBill].used.data }}</li>                  
+                </ul>
+              </td>
+            </tr>
+          </tbody>
+        </table>            
+      </div>
       <div class="content">    
-        <div v-for="bill in bills" :key="bill.id">
-          {{ bill.id }}
+        <div v-for="(value, key) in bills" style="margin-bottom: 5px;">
+          {{ value.id }} <button class="button is-small is-primary" @click="showBillDetails(key)">Show Bill</button>
         </div>
       </div>
     </div>
   `,
-  data: () => ({
-    bills: null
-  }),  
-  created: function () {
-    this.fetchData()
-  },  
-  methods: {
-    fetchData() {
-      var route = config.apiURL + '/bills.json';
-
-      this.$http.get(route).then((response) => {
-        this.bills = response.body.bills;
-      });
+  props: ["bills"],
+  data: function () {
+    return {
+      loadBill: "0"
     }
-  }  
+  },
+  methods: {
+    showBillDetails(key) {
+      this.loadBill = key;
+    }
+  }   
 })
 
 Vue.component('devices-section', {
@@ -84,21 +88,7 @@ Vue.component('devices-section', {
       </div>
     </div>
   `,
-  data: () => ({
-    devices: null
-  }),  
-  created: function () {
-    this.fetchData()
-  },  
-  methods: {
-    fetchData() {
-      var route = config.apiURL + '/devices.json';
-
-      this.$http.get(route).then((response) => {
-        this.devices = response.body.devices;
-      });
-    }
-  }  
+  props: ["devices"]
 })
 
 Vue.component('contract-section', {
@@ -110,25 +100,88 @@ Vue.component('contract-section', {
       </div>
     </div>
   `,
-  data: () => ({
-    contract: null
-  }),  
-  created: function () {
-    this.fetchData()
-  },  
-  methods: {
-    fetchData() {
-      var route = config.apiURL + '/contract.json';
+  props: ["contract"]
+})
 
-      this.$http.get(route).then((response) => {
-        this.contract = response.body.contract;
-      });
-    }
-  }  
+Vue.component('last-months-usages-section', {
+  template: `
+    <div v-if="contract">
+      <p class="title">Your usage last month...</p>
+
+      <nav class="level is-mobile">
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Tweets</p>
+            <p class="title">3,456</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Following</p>
+            <p class="title">123</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Followers</p>
+            <p class="title">456K</p>
+          </div>
+        </div>
+        <div class="level-item has-text-centered">
+          <div>
+            <p class="heading">Likes</p>
+            <p class="title">789</p>
+          </div>
+        </div>
+      </nav>
+
+    </div>
+  `,
+  props: ["bills", "contract"]
 })
 
 new Vue({
   el: '#app',
   data: {
-  }
+    bills: [],
+    devices: [],
+    contract: {},
+    customer: {}    
+  },
+  mounted: function () {
+    this.getBills();
+    this.getDevices();        
+    this.getContract();
+    this.getCustomer();    
+  },  
+  methods: {
+    getBills() {
+      var route = config.apiURL + '/bills.json';
+
+      this.$http.get(route).then((response) => {
+        this.bills = response.body.bills;
+      });
+    },
+    getDevices() {
+      var route = config.apiURL + '/devices.json';
+
+      this.$http.get(route).then((response) => {
+        this.devices = response.body.devices;
+      });
+    },
+    getContract() {
+      var route = config.apiURL + '/contract.json';
+
+      this.$http.get(route).then((response) => {
+        this.contract = response.body.contract;
+      });
+    },
+    getCustomer() {
+      var route = config.apiURL + '/customer.json';
+
+      this.$http.get(route).then((response) => {
+        this.customer = response.body.customer;
+      });
+    }            
+  }    
 })
