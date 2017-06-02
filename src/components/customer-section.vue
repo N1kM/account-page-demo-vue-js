5946031aa64f72cf277dad028c915bc12eb51aee
N1kM <n.morgan@LeadDigitaliMac.local>
2017-06-02 11:57:29 +0100

    removed customer section component from js file and put it into it’s own component file

---
+++ src/components/customer-section.vue
@@ -0,0 +1,31 @@
+<template>
+  <div v-if="customer">
+    <div class="content">
+      <div class="media" style="margin-bottom: 20px;">
+        <div class="media-left">
+          <figure class="image" style="height: 40px; width: 40px;">
+            <img v-bind:src="customer.avatar">
+          </figure>
+        </div>
+        <div class="media-content">
+          <p class="title is-4">{{ customer.firstName }} {{ customer.lastName }}</p>
+          <p class="subtitle is-6">{{ customer.username }}</p>
+          <hr />
+          <p>Email: {{ customer.email }}</p>
+          <p>Address:<br />{{ customer.addressLine1 }}, <span v-if="customer.addressLine2">{{ customer.addressLine2 }},</span> {{ customer.town }}<span v-if="customer.county">, {{ customer.county }}</span><br />{{ customer.country }}, {{ customer.postcode }}</p>
+        </div>
+      </div>
+      <p class="title">Manage how we notify you</p>
+      <pre>{{ customer.notifications }}</pre>
+      <ul v-for="(type, key) in customer.notifications">
+        <li><button class="button is-small" v-on:click="toggleActive(key, type)" v-bind:class="{ 'is-success': type}">{{ key | titleCase }}</button></li>
+      </ul>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'CustomerSection'
+}
+</script>
--- src/main.js
+++ src/main.js
@@ -1,6 +1,7 @@
 import Vue from 'vue'
 import VueResource from 'vue-resource'
 import Moment from 'moment'
+import CustomerSection from './components/customer-section.vue'
 
 // Calls 'VueResource.install(Vue)'
 Vue.use(VueResource);
@@ -9,32 +10,9 @@ var config = {
   apiURL: window.location.origin + '/api'
 }
 
-Vue.component('customer-section', {
-  template: `
-    <div v-if="customer">
-      <div class="content">
-        <div class="media" style="margin-bottom: 20px;">
-          <div class="media-left">
-            <figure class="image" style="height: 40px; width: 40px;">
-              <img v-bind:src="customer.avatar">
-            </figure>
-          </div>
-          <div class="media-content">
-            <p class="title is-4">{{ customer.firstName }} {{ customer.lastName }}</p>
-            <p class="subtitle is-6">{{ customer.username }}</p>
-            <hr />
-            <p>Email: {{ customer.email }}</p>
-            <p>Address:<br />{{ customer.addressLine1 }}, <span v-if="customer.addressLine2">{{ customer.addressLine2 }},</span> {{ customer.town }}<span v-if="customer.county">, {{ customer.county }}</span><br />{{ customer.country }}, {{ customer.postcode }}</p>
-          </div>
-        </div>
-        <p class="title">Manage how we notify you</p>
-        <pre>{{ customer.notifications }}</pre>
-        <ul v-for="(type, key) in customer.notifications">
-          <li><button class="button is-small" v-on:click="toggleActive(key, type)" v-bind:class="{ 'is-success': type}">{{ key | titleCase }}</button></li>
-        </ul>
-      </div>
-    </div>
-  `,
+new Vue({
+  el: 'customer-section',
+  components: { CustomerSection },
   props: ["customer"],
   methods: {
     toggleActive: function(key, type){
